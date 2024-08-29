eb61e810a38cb030fbcb98b6d262b4e662fa46ee
prakash <54275178+prakashbhagat@users.noreply.github.com>
2024-08-29 22:08:13 +0530

    Revert "Feat/cart fixed"

--- src/components/CartLogin.tsx
+++ src/components/CartLogin.tsx
@@ -1,5 +1,5 @@
-const LoginPage = () => (
-    <div className="flex flex-col justify-center items-center  px-4 mb-16">
+const LoginPage = ( ) => (
+    <div className="flex flex-col justify-center items-center min-h-screen px-4 mb-16">
         <div className="sm:w-full sm:max-w-sm">
             <h2 className="mt-2 text-left text-base md:text-base lg:text-lg font-bold leading-9 tracking-tight text-gray-900">
                 Existing User?
@@ -8,20 +8,20 @@ const LoginPage = () => (
         </div>
         <div className="mt-4 sm:w-full sm:max-w-sm">
             <form className="space-y-5" action="#" method="POST">
-                {/* <div>
+                <div>
                     <label className="text-xl block">Email</label>
-                    <input
-                        type="text"
+                    <input 
+                        type="text" 
                         placeholder="Email"
-                        className="px-4 py-3 text-sm rounded-md bg-white border border-gray-400 w-full outline-blue-500"
+                        className="px-4 py-3 text-sm rounded-md bg-white border border-gray-400 w-full outline-blue-500" 
                     />
-                </div> */}
+                </div>
                 <div>
                     <label className="text-sm block">Password</label>
-                    <input
-                        type="password"
+                    <input 
+                        type="password" 
                         placeholder="Password"
-                        className="px-4 py-3 text-sm rounded-md bg-white border border-gray-400 w-full outline-blue-500"
+                        className="px-4 py-3 text-sm rounded-md bg-white border border-gray-400 w-full outline-blue-500" 
                     />
                 </div>
                 <div>
@@ -34,7 +34,10 @@ const LoginPage = () => (
                         Log In
                     </button>
                 </div>
-
+                <div className="text-center text-sm text-gray-500">
+                    New to NowDigitalEasy?
+                    <a href="#" className="font-bold leading-6 text-blue-600">Sign up here</a>
+                </div>
             </form>
         </div>
     </div>
